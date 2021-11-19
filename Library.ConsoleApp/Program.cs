63c033d5ad826c12d14740d842c539cade51d3ea
Bartłomiej Cichewicz <bartlomiej.cichewicz@gmail.com>
2021-11-19 20:11:18 +0100

    Warsztat - Zadanie 3

--- Library.ConsoleApp/Program.cs
+++ Library.ConsoleApp/Program.cs
@@ -18,6 +18,28 @@ namespace Library.ConsoleApp
                 Console.WriteLine("Access Granted");
             else
                 Console.WriteLine("Access Denied");
+            string input;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Menu:");
+                Console.WriteLine("[dodaj] [usun] [wypisz] [zmien] [dodaj zamowienie] [lista zamowien]");
+                input = Console.ReadLine();
+                if (input == "dodaj")
+                    Console.WriteLine("proba dodania ksiazki");
+                else if (input == "usun")
+                    Console.WriteLine("proba usuniecia ksiazki");
+                else if (input == "wypisz")
+                    Console.WriteLine("proba wypisania wszystkich ksiazek");
+                else if (input == "zmien")
+                    Console.WriteLine("proba zmiany stanu magazynowego ksiazek");
+                else if (input == "dodaj zamowienie")
+                    Console.WriteLine("proba dodania nowego zamowienia");
+                else if (input == "lista zamowien")
+                    Console.WriteLine("proba wypisania wszystkich zamowien");
+                else
+                    Console.WriteLine("wybrana komenda nie jest wspierana");
+            } while (input != "wyjdz"); 
         }
     }
 }
