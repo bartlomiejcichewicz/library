da4331b964d4970dd2ecbcf9838f672583d04c2f
Bartłomiej Cichewicz <bartlomiej.cichewicz@gmail.com>
2021-11-19 19:01:16 +0100

    Warsztat - Zadanie 2

--- Library.ConsoleApp/Program.cs
+++ Library.ConsoleApp/Program.cs
@@ -10,6 +10,14 @@ namespace Library.ConsoleApp
         {
             Book book = new Book("Hobbit", "J. R. R. Tolkien", 1937, "12345", 15, 19.99m);
             BooksRepository repository = new BooksRepository();
+            Console.Write("Login: ");
+            string login = Console.ReadLine();
+            Console.Write("Password: ");
+            string password = Console.ReadLine();
+            if (login == "Admin" && password == "password")
+                Console.WriteLine("Access Granted");
+            else
+                Console.WriteLine("Access Denied");
         }
     }
 }
