c29f6b40b53da90d5cfe5bec59f7a507825ee0e2
Bartłomiej Cichewicz <bartlomiej.cichewicz@gmail.com>
2021-11-19 18:51:03 +0100

    Warszttat - Zadanie 1

---
+++ Library.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using Library.Domain;
+using Library.Persistance;
+using System;
+
+namespace Library.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("Hobbit", "J. R. R. Tolkien", 1937, "12345", 15, 19.99m);
+            BooksRepository repository = new BooksRepository();
+        }
+    }
+}
---
+++ Library.Persistance/BooksRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Persistance
+{
+    public class BooksRepository
+    {
+    }
+}
